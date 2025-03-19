168b8143a9cffe5b999594366979f8cd0d65e58c
Avanigaikwad <gaikwadavani27@gmail.com>
2025-03-19 16:36:05 +0530

    Updated changes in file rename Route.jsx to route.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
-import ApppRoutes from './config/xyz.jsx'
+import ApppRoutes from './config/routes.jsx'
 import { Toaster } from 'react-hot-toast'
 import { ChatProvider } from './context/ChatContext.jsx'
 
