d6227a6a824d5557a63dffca49fc7c98feaf7449
Avanigaikwad <gaikwadavani27@gmail.com>
2025-03-21 16:58:25 +0530

    Updated the project dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: false, // Disable minification to debug generated files
+    sourcemap: true, // Generate source maps for debugging
+  
+}})
+
+
