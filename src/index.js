8c31c0642a6164d421bbc45ff9fbd0295c939107
Avanigaikwad <gaikwadavani27@gmail.com>
2025-03-21 15:22:04 +0530

    Updated folders to make app production ready

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from "./App.jsx";
 import "./index.css"; // If you have global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+if (window.attachEvent) {
+    window.attachEvent = () => console.warn("attachEvent is deprecated");
+  }
+  
